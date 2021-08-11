04ec5a334ed28113c08bfd12e616bfc748c1073d
rhino <rhino_luo@163.com>
2021-08-11 16:37:59 +0800

    Update HttpUrl.java

--- app/src/main/java/com/rhino/http/demo/http/HttpUrl.java
+++ app/src/main/java/com/rhino/http/demo/http/HttpUrl.java
@@ -10,19 +10,11 @@ public class HttpUrl {
      * 发布地址
      */
     private static final String BASE_URL = "https://xxx/";
-    /**
-     * UAT测试地址
-     */
-//    private static final String BASE_URL = "http://asdatapro.csuat.cmbchina.cn/";
-    /**
-     * 测试地址
-     */
-//    private static final String BASE_URL = "http://99.13.197.52:8080/";
 
     /**
      * 获取token
      */
-    public static final String URL_GET_TOKEN = BASE_URL + "data/token/checkTokenMobile";
+    public static final String URL_GET_TOKEN = BASE_URL + "xxxx";
 
 
 
